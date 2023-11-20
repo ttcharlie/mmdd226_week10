d8e33162ba6bbdb33dab198a903164ef2adb66e0
ttcharlie <126117963+ttcharlie@users.noreply.github.com>
2023-11-20 13:41:16 -0500

    3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-var myModels = ["/models/orange.glb", "/models/teal.glb"]
-var counter = 0;
+// simple change model
+document.getElementById("change-colour").addEventListener("click", function() {
+    document.getElementById("model").src = "/models/teal.glb";
+});
 
-document.getElementById("change-colour"),addEventListener("click", function(){
+// example with showing content
+document.getElementById("change-colour").addEventListener("click", function() {
+    document.getElementById("model-info").style.display = "block";
+});
+
+// example with multiple models
+var myModels = ["/models/orange.glb", "/models/teal.glb", "/models/orange.glb", "/models/teal.glb",]
+var counter = 0;
+document.getElementById("change-colour").addEventListener("click", function() {
     counter++;
-    if (counter==3){
+    if (counter==3) { 
         counter = 0;
     }
     document.getElementById("model").src = myModels[counter];
-    document.getElementById("model-info").style.display = "block";
-
 });
